4bfe50a1e28326a852a6f7437c85e44a132fed6f
VickyDarmana <134504626+VickyDarmana@users.noreply.github.com>
2024-02-26 20:43:04 +0700

    update

--- app/src/main/java/edu/uph/uts_pmd_vickydarmana/profil.java
+++ app/src/main/java/edu/uph/uts_pmd_vickydarmana/profil.java
@@ -2,16 +2,42 @@ package edu.uph.uts_pmd_vickydarmana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 
 public class profil extends AppCompatActivity {
+
+    LinearLayout button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+        button1 = (LinearLayout) findViewById(R.id.button1);
+        button1.setOnClickListener(v -> {
+            Log.d("Submit", "Masuk ke tampilanawal");
+            Intent intent = new Intent(profil.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
+
+//    @Override
+//    public <View> void onClick(View view){
+//        showToast();
+//    }
+//
+//    private void showToast(){
+//        String studentid = studentid.getText().toString();
+//        String nama = nama.getText().toString();
+//        String jurusan = jurusan.getText().toString();
+//        String tahunmasuk = tahunmasuk.getText().toString();
+//        String status = status.getText().toString();
+//        String kampus = kampus.getText().toString();
+//
+//        String message = "StudentID: " + studentid + " | "Nama: " + nama + " | "Jurusan: " + jurusan + " | "TahunMasuk: " + tahunmasuk + " | "Status: " + status + " | "Kampus: " + kampus;
+//    }
 }
 
 
